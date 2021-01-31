eb26beb4a164876e3be9d7616bb157594c323839
Maxim Tishchenko <krepkiyvitya@gmail.com>
2021-01-31 12:42:19 +0700

    restart level button

---
+++ Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    public void DoRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
