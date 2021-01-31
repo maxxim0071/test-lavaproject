bc33e3bc65857db1a83654b974cb8b619e29ba7d
Maxim Tishchenko <krepkiyvitya@gmail.com>
2021-01-31 07:23:18 +0700

    zombie patrol feature

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,10 +2,33 @@
 
 public class Enemy : Entity
 {
+    Transform[] points;
+    int destination = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        points = GameObject.Find("WaypointsContainer").GetComponent<WaypointsContainer>().points;
+    }
+
     protected override void Update()
     {
         base.Update();
 
-        
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+    }
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+
+        agent.destination = points[destination].position;
+
+        destination = (destination + 1) % points.Length;
     }
 }
